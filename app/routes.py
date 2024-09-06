2f9c1a33ae61c43893737ca9c2f6df209f05a872
Aninxo <lawrencesanyah298@gmail.com>
2024-09-07 09:43:04 +1000

    Initial commit

---
+++ app/routes.py
@@ -0,0 +1,11 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', title='Home')
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title='Login')
